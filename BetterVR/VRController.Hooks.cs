861b5d3a11f2e2e9ed274839ffb0be27b6a28692
KhLTz <151600406+KhLTz@users.noreply.github.com>
2023-11-21 04:27:46 +0000

    Fix thumbstick input

--- BetterVR/VRController.Hooks.cs
+++ BetterVR/VRController.Hooks.cs
@@ -1,4 +1,6 @@
+using HTC.UnityPlugin.Vive;
 using HarmonyLib;
+using UnityEngine;
 
 namespace BetterVR
 { 
@@ -24,9 +26,11 @@ namespace BetterVR
 
             //If the pointer game object is active, then set the cursor angle
             if (BetterVRPlugin.debugLog) BetterVRPlugin.Logger.LogInfo($" LaserPointer L active, setting angle to {BetterVRPlugin.SetVRControllerPointerAngle.Value}");    
-            pluginInstance.StartCoroutine(
-                VRControllerPointer.SetAngleAfterTime(BetterVRPlugin.SetVRControllerPointerAngle.Value, BetterVRPluginHelper.VR_Hand.left)
-            );
+            
+            // Not working currently.
+            // pluginInstance.StartCoroutine(
+            //    VRControllerPointer.SetAngleAfterTime(BetterVRPlugin.SetVRControllerPointerAngle.Value, BetterVRPluginHelper.VR_Hand.left)
+            // );
                     
         }
 
@@ -38,10 +42,34 @@ namespace BetterVR
 
             //If the pointer game object is active, then set the cursor angle
             if (BetterVRPlugin.debugLog) BetterVRPlugin.Logger.LogInfo($" LaserPointer R active, setting angle to {BetterVRPlugin.SetVRControllerPointerAngle.Value}");
-            pluginInstance.StartCoroutine(
-                VRControllerPointer.SetAngleAfterTime(BetterVRPlugin.SetVRControllerPointerAngle.Value, BetterVRPluginHelper.VR_Hand.right)
-            );
+
+            // Not working currently.
+            // pluginInstance.StartCoroutine(
+            //    VRControllerPointer.SetAngleAfterTime(BetterVRPlugin.SetVRControllerPointerAngle.Value, BetterVRPluginHelper.VR_Hand.right)
+            // );
         }
 
+        [HarmonyPrefix, HarmonyPatch(typeof(HScene), "GetAxis")]
+        public static bool PatchGetAxis(HScene __instance, HandRole _hand, ref Vector2 __result)
+        {
+            if (_hand != HandRole.RightHand)
+            {
+                // For safety, fall back to vanilla logic in left hand as a workaround in case there is something missing in this patch.
+                return true;
+            }
+
+            // This method works for Oculus controllers' thumbsticks too.
+            var axis = ViveInput.GetPadAxisEx<HandRole>(HandRole.RightHand);
+
+            if (axis == Vector2.zero)
+            {
+                return true;
+            }
+
+            // The vanilla pad/thumb stick detection is half broken and does not work on some platforms, giving rise to the necessity of this patch.
+            __result = axis;
+
+            return false;
+        }
     }
 }
