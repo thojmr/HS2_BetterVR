0b785793efa419defd1251debd9983d7dd23828c
KhLTz <151600406+KhLTz@users.noreply.github.com>
2023-11-21 04:32:38 +0000

    Adjustable player scale

--- BetterVR/GUI/BetterVRPlugin.Config.cs
+++ BetterVR/GUI/BetterVRPlugin.Config.cs
@@ -7,9 +7,12 @@ namespace BetterVR
     {
         public static ConfigEntry<bool> EnableControllerColliders { get; private set; }
         public static ConfigEntry<float> SetVRControllerPointerAngle { get; private set; }
+
+        public static ConfigEntry<float> PlayerScale { get; private set; }
         public static ConfigEntry<bool> SqueezeToTurn { get; private set; }
         public static ConfigEntry<bool> FixWorldSizeScale { get; private set; }
         public static ConfigEntry<bool> MultipleRandomHeroine { get; private set; }
+        public static ConfigEntry<bool> UsePrivacyScreen { get; private set; }
 
 
         /// <summary>
@@ -25,6 +28,11 @@ namespace BetterVR
             SqueezeToTurn = Config.Bind<bool>("VR General", "Squeeze to Turn", true, 
                 new ConfigDescription("Allows you to turn the headset with hand rotation while zqueezing the controller."));
 
+            PlayerScale = Config.Bind<float>("VR General", "Player Scale", 1.15f, 
+                 new ConfigDescription("Player scale when fixing world size scale, default is 1.15",
+                 new AcceptableValueRange<float>(0.5f, 2f)));
+            PlayerScale.SettingChanged += FixWorldSizeScale_SettingsChanged;
+
             // SetVRControllerPointerAngle = Config.Bind<float>("VR General", "(Not working yet)Laser Pointer Angle", 0, 
             //     new ConfigDescription("0 is the default angle, and negative is down.",
             //     new AcceptableValueRange<float>(-90, 90)));
@@ -35,9 +43,12 @@ namespace BetterVR
             FixWorldSizeScale.SettingChanged += FixWorldSizeScale_SettingsChanged; 
 
             MultipleRandomHeroine = Config.Bind<bool>("VR General", "Multiple Heroine when Random", false, 
-                new ConfigDescription("Will add 2 Heroine to the HScene when the 'Random' button is selected. (Default is 1)"));           
+                new ConfigDescription("Will add 2 Heroine to the HScene when the 'Random' button is selected. (Default is 1)"));
 
-        }      
+            UsePrivacyScreen = Config.Bind<bool>("VR General", "Use Privacy Screen", true,
+                new ConfigDescription("Puts a black screen on desktop window"));
+            UsePrivacyScreen.SettingChanged += UsePrivacyScreen_SettingsChanged;
+        }
 
 
         /// <summary>
@@ -67,6 +78,9 @@ namespace BetterVR
             BetterVRPluginHelper.FixWorldScale(FixWorldSizeScale.Value);
         }
 
-
+        internal void UsePrivacyScreen_SettingsChanged(object sender, System.EventArgs e)
+        {
+            BetterVRPluginHelper.UpdatePrivacyScreen();
+        }
     }
 }
