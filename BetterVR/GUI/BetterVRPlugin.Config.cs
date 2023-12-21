ce745c35ecff9353970f9d0004d780169349b951
KhLTz <151600406+KhLTz@users.noreply.github.com>
2023-12-21 01:27:41 +0000

    Update BetterVRPlugin.Config.cs

--- BetterVR/GUI/BetterVRPlugin.Config.cs
+++ BetterVR/GUI/BetterVRPlugin.Config.cs
@@ -1,19 +1,25 @@
 using BepInEx.Configuration;
 using HarmonyLib;
+using UnityEngine;
 
 namespace BetterVR 
 {
-    public partial class BetterVRPlugin 
+    public partial class BetterVRPlugin
     {
         public static ConfigEntry<bool> EnableControllerColliders { get; private set; }
+        public static ConfigEntry<string> GestureStrip { get; private set; }
         public static ConfigEntry<float> SetVRControllerPointerAngle { get; private set; }
-
-        public static ConfigEntry<float> PlayerScale { get; private set; }
-        public static ConfigEntry<bool> SqueezeToTurn { get; private set; }
+        public static ConfigEntry<float> PlayerLogScale { get; private set; }
+        public static ConfigEntry<string> SqueezeToTurn { get; private set; }
+        public static ConfigEntry<bool> AllowVerticalRotation { get; private set; }
         public static ConfigEntry<bool> FixWorldSizeScale { get; private set; }
         public static ConfigEntry<bool> MultipleRandomHeroine { get; private set; }
         public static ConfigEntry<bool> UsePrivacyScreen { get; private set; }
 
+        public static float PlayerScale {
+            get { return Mathf.Pow(2, PlayerLogScale.Value); }
+            set { PlayerLogScale.Value = Mathf.Log(value, 2); }
+        }
 
         /// <summary>
         /// Init the Bepinex config manager options
@@ -23,15 +29,29 @@ namespace BetterVR
 
             EnableControllerColliders = Config.Bind<bool>("VR General", "Enable Controller Colliders (boop!)", true, 
                 "Allows collision of VR controllers with all dynamic bones");
-            EnableControllerColliders.SettingChanged += EnableControllerColliders_SettingsChanged;  
+            EnableControllerColliders.SettingChanged += EnableControllerColliders_SettingsChanged;
 
-            SqueezeToTurn = Config.Bind<bool>("VR General", "Squeeze to Turn", true, 
-                new ConfigDescription("Allows you to turn the headset with hand rotation while zqueezing the controller."));
+            GestureStrip = Config.Bind<string>(
+                "VR General", "Enable Gesture Strip", "Right hand",
+                new ConfigDescription(
+                    "Enable holding trigger and dragging to change cloth states.",
+                    new AcceptableValueList<string>(new string[] { "Disabled", "Left hand", "Right hand" })));
 
-            PlayerScale = Config.Bind<float>("VR General", "Player Scale", 1.15f, 
-                 new ConfigDescription("Player scale when fixing world size scale, default is 1.15",
-                 new AcceptableValueRange<float>(0.5f, 2f)));
-            PlayerScale.SettingChanged += FixWorldSizeScale_SettingsChanged;
+            SqueezeToTurn = Config.Bind<string>(
+                "VR General", "Squeeze to Turn", "One-handed",
+                new ConfigDescription(
+                    "Allows you to turn the headset with hand rotation while squeezing the controller.",
+                    new AcceptableValueList<string>(new string[] { "Disabled", "One-handed", "Two-handed"})));
+
+            AllowVerticalRotation = Config.Bind<bool>(
+                "VR General", "Allow Vertical Rotation", false, new ConfigDescription("Allows rotating the world vertically."));
+
+            PlayerLogScale = Config.Bind<float>(
+                "VR General", "Log2 of Player Scale", Mathf.Log(1.15f, 2f), 
+                 new ConfigDescription(
+                     "Log2 of player scale when fixing world size scale, default is Log2(1.15)",
+                     new AcceptableValueRange<float>(-4, 4)));
+            PlayerLogScale.SettingChanged += FixWorldSizeScale_SettingsChanged;
 
             // SetVRControllerPointerAngle = Config.Bind<float>("VR General", "(Not working yet)Laser Pointer Angle", 0, 
             //     new ConfigDescription("0 is the default angle, and negative is down.",
@@ -50,7 +70,6 @@ namespace BetterVR
             UsePrivacyScreen.SettingChanged += UsePrivacyScreen_SettingsChanged;
         }
 
-
         /// <summary>
         /// On config options changed by user, trigger stuff
         /// </summary>
@@ -66,12 +85,10 @@ namespace BetterVR
             }
         }
 
-
         // internal void SetVRControllerPointerAngle_SettingsChanged(object sender, System.EventArgs e) 
         // {
         //     VRControllerPointer.UpdateOneOrMoreCtrlPointers(SetVRControllerPointerAngle.Value);
         // }
-
 
         internal void FixWorldSizeScale_SettingsChanged(object sender, System.EventArgs e) 
         {
