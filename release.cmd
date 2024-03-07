e6817b61ef8877eabe7c4e9cd40770a0e3f0edde
KhLTz <151600406+KhLTz@users.noreply.github.com>
2024-03-07 05:10:48 +0000

    Update release.cmd

--- release.cmd
+++ release.cmd
@@ -7,7 +7,7 @@ set name=HS2_BetterVR
 IF NOT EXIST  "../releases/%name%/BepInEx/plugins" MKDIR  "../releases/%name%/BepInEx/plugins"
 COPY "../bin/%name%/BepinEx/plugins\%name%.dll" "../releases/%name%/BepInEx/plugins"
 
-IF EXIST "%ProgramFiles%\7-Zip\7z.exe" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "../releases/%name% v%version%.zip" "../releases/%name%" -mx0
-IF EXIST "C:\Program Files\7-Zip\7z.exe" "C:\Program Files\7-Zip\7z.exe" a -tzip "../releases/%name% v%version%.zip" "../releases/%name%" -mx0
+IF EXIST "%ProgramFiles%\7-Zip\7z.exe" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "../releases/%name%.v%version%.zip" "../releases/%name%" -mx0
+IF EXIST "C:\Program Files\7-Zip\7z.exe" "C:\Program Files\7-Zip\7z.exe" a -tzip "../releases/%name%.v%version%.zip" "../releases/%name%" -mx0
 
 start .\..\releases
